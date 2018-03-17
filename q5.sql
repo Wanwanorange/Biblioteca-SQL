9bd2ee550cc1c521e404c9f924d888a1aeeb001b
wanwanorange <wanchen.y95@gmail.com>
2018-03-17 11:38:10 -0500

    In the process of fixing up q5 to add up book and movie selections for each user

--- q5.sql
+++ q5.sql
@@ -1,15 +1,19 @@
 -- Who has checked out more than 1 item? 
 -- Tip: Research the GROUP BY syntax.
 
-select member.name, book.title
+-- select member.name, sum(count(book.title), count(movie.title)) 
+
+select member.name, count(book.title) 
 	from member, book, checkout_item
 	where 
-	checkout_item.member_id = member.id and
-	checkout_item.book_id = book.id 
+	(checkout_item.member_id = member.id and
+	checkout_item.book_id = book.id) 
+	group by member.name
 union
-select member.name, movie.title
+select member.name, count(movie.title) 
 	from member, movie, checkout_item
 	where 
-	checkout_item.member_id = member.id and
-	checkout_item.movie_id = movie.id
+	(checkout_item.member_id = member.id and
+	checkout_item.movie_id = movie.id) 
 	group by member.name;
+
