0b9de2409bed12b64a6f851be4acb5c7a881cca7
wanwanorange <wanchen.y95@gmail.com>
2018-03-18 20:41:07 -0500

    Fixed q5 to get members who borrowed more than one item

--- q5.sql
+++ q5.sql
@@ -3,17 +3,10 @@
 
 -- select member.name, sum(count(book.title), count(movie.title)) 
 
-select member.name, count(book.title) 
-	from member, book, checkout_item
-	where 
-	(checkout_item.member_id = member.id and
-	checkout_item.book_id = book.id) 
+select member.name
+ from checkout_item join member
+	on checkout_item.member_id = member.id
 	group by member.name
-union
-select member.name, count(movie.title) 
-	from member, movie, checkout_item
-	where 
-	(checkout_item.member_id = member.id and
-	checkout_item.movie_id = movie.id) 
-	group by member.name;
+	having count(member.name) > 1;
+
 
